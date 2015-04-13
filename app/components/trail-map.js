1bdd63d04443626f917af3f374dd4ba5e7abed29
Lydias303 <shaferlydia@yahoo.com>
2015-04-13 16:09:44 -0600

    go the search working

--- app/components/trail-map.js
+++ app/components/trail-map.js
@@ -1,14 +1,20 @@
+/* globals L */
+
 import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  map: null,
   classNames: ['trail-map'],
 
   didInsertElement: function () {
 
     L.mapbox.accessToken = 'pk.eyJ1IjoibHlkaWFzMzAzIiwiYSI6ImM0WG9rY28ifQ.bM1Nx1fsDmbAFLVP1f9Img';
 
-    var map = L.mapbox.map(this.elementId, 'lydias303.4be232dc');
+    let map = L.mapbox.map(this.elementId, 'lydias303.4be232dc');
+    this.set('map', map);
+
+    map.setView([this.get('trail.lat'), this.get('trail.lng')], 15);
   }
 
 });
