eaae839deb4c4e2c0299a6834a71762f924f47b2
Lydias303 <shaferlydia@yahoo.com>
2015-04-13 21:33:15 -0600

    added search bar styling, added icon, dont love the color, think i will change it, turned off autocomplete with the search

--- Brocfile.js
+++ Brocfile.js
@@ -3,10 +3,11 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
-
-app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
-  destDir: 'assets'
-});
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+// app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
+//   destDir: 'assets'
+// });
 
 app.import('bower_components/mapbox.js/mapbox.standalone.js');
 app.import('bower_components/mapbox.js/mapbox.standalone.css');
