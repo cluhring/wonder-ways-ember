e21927a6e059cbea127210fa85fb41c4ea72fbdb
Lydias303 <shaferlydia@yahoo.com>
2015-04-14 22:19:59 -0600

    added length and description to acticity display

--- app/controllers/trail.js
+++ app/controllers/trail.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  firstActivity: Ember.computed.alias('model.activities'),
+  trailActivities: Ember.computed.alias('model.activities'),
 
   actions: {
     filter: function(category) {
