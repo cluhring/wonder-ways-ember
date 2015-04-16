7e31fe5585d93602f821f9956b3fc1d582448cf6
Lydias303 <shaferlydia@yahoo.com>
2015-04-15 19:55:55 -0600

    added links to each state

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -54,21 +54,4 @@ export default Ember.Controller.extend({
   "West Virginia",
   "Wisconsin",
   "Wyoming"],
-
-  filteredTrails: function () {
-    let searchTerm = this.get('searchTerm').toLowerCase();
-    let trails = this.get('model');
-
-    if (searchTerm) {
-      return trails.filter((trail) => {
-        let keys = Object.keys(trail);
-        let isMatching = false;
-        keys.forEach((key) => {
-          if (trail[key] && trail[key].toString().toLowerCase().includes(searchTerm)) { isMatching = true; }
-        });
-        return isMatching;
-      });
-    }
-    return trails;
-  }.property('searchTerm', 'model')
 });
